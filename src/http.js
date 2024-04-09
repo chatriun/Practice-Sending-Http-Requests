54c65c118db9e680dbefa9e0bd740b723ec9af61
salysaly <anyawee.sr@gmail.com>
2024-04-09 14:39:56 +0700

    ERROR: Failed to fetch

--- src/http.js
+++ src/http.js
@@ -1,10 +1,28 @@
 export async function fetchAvailablePlace() {
   const response = await fetch("http://localhost:3000/places");
-    const resData = await response.json();
+  const resData = await response.json();
 
-    if (!response.ok) {
-      throw new Error(`error to fetch data`);
+  if (!response.ok) {
+    throw new Error(`error to fetch data`);
+  }
+
+  return resData.places
+}
+
+export async function updateUserPlaces(places){
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({places}),
+    headers: {
+      "Content-Type": "application/json"
     }
+  })
 
-    return resData.places
+  const resData = await response.json()
+
+  if (!response.ok){
+    throw new Error(`fail to update user data`)
+  }
+
+  return resData.message
 }
